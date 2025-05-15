bcac85b5fb1f4da091a88638a12ce0cd9f42036d
Don Kirkham <donkirkham@live.com>
2025-05-15 11:45:06 -0500

    Add new image assets for team profiles
    - Added outline PNG for team with ID 50e84d05-be1b-4769-8022-2e228806c99c.
    - Added color PNG for team with ID b09d4e84-b76f-4c32-b8cf-a258d9fe4ea4, including metadata.
    - Added outline PNG for team with ID b09d4e84-b76f-4c32-b8cf-a258d9fe4ea4.

---
+++ src/webparts/lifecycleClassic/components/ILifecycleClassicProps.ts
@@ -0,0 +1,7 @@
+export interface ILifecycleClassicProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/lifecycleHooks/components/ILifecycleHooksProps.ts
@@ -0,0 +1,7 @@
+export interface ILifecycleHooksProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
